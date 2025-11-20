9f2a711a25e39a23cfe82f325efc165ee282ef27
Donia Emad <donia@example.com>
2025-11-20 17:17:31 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const faders = document.querySelectorAll('.fade-up');
+
+    const appearOptions = {
+      threshold: 0, 
+      rootMargin: "0px 0px -100px 0px" 
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          entry.target.classList.add('show');
+          appearOnScroll.unobserve(entry.target);
+        }
+      });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+    });
+
+
+    const navbar = document.getElementById('mainNavbar');
+    const scrollTrigger = 100; 
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= scrollTrigger) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
+});
+
+document.addEventListener('click', function (e) {
+    const navbar = document.querySelector('.navbar-collapse');
+    const toggleBtn = document.querySelector('.navbar-toggler');
+
+
+    if (navbar.classList.contains('show')) {
+
+
+        if (!navbar.contains(e.target) && !toggleBtn.contains(e.target)) {
+            toggleBtn.click();  
+        }
+    }
+
+});
